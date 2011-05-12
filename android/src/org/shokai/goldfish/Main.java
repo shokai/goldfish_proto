14924415a03a790f67ca5d6af7a8319297ed557c
Sho Hashimoto <hashimoto@shokai.org>
2011-05-12 16:17:17 +0900

    work on android2.3.4

--- android/src/org/shokai/goldfish/Main.java
+++ android/src/org/shokai/goldfish/Main.java
@@ -1,6 +1,9 @@
 package org.shokai.goldfish;
 
-import jp.co.topgate.android.nfc.TagWrapper;
+import java.lang.reflect.Field;
+
+import com.google.common.primitives.UnsignedBytes;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
@@ -28,13 +31,21 @@ public class Main extends Activity {
         if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)) {
             try{
                 Parcelable tag = intent.getParcelableExtra("android.nfc.extra.TAG");
-                TagWrapper tw = new TagWrapper(tag);
-                String id = tw.getHexIDString();
+                Field f = tag.getClass().getDeclaredField("mId");
+                f.setAccessible(true);
+                byte[] mId = (byte[]) f.get(tag);
+                StringBuilder sb = new StringBuilder();
+                for (byte id : mId) {
+                    String hexString = Integer.toHexString(UnsignedBytes.toInt(id));
+                    if (hexString.length() == 1) sb.append("0");
+                    sb.append(hexString);
+                }
+                String id = sb.toString();
                 textViewTag.setText("TAG : "+id);
                 trace(id);
             }
             catch(Exception e){
-                trace(e);
+                e.printStackTrace();
                 textViewTag.setText("TAG error");
             }
         }
@@ -43,4 +54,5 @@ public class Main extends Activity {
     void trace(Object message){
         Log.v("GoldFish", message.toString());
     }
+    
 }
