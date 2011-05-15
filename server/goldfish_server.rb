71bffa9b7ff7bc0a09d5f5614d09f8984f676574
Sho Hashimoto <hashimoto@shokai.org>
2011-05-16 01:56:40 +0900

    http api

--- server/goldfish_server.rb
+++ server/goldfish_server.rb
@@ -1,29 +1,32 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'eventmachine'
+require 'evma_httpserver'
 require 'ArgsParser'
 
 parser = ArgsParser.parser
 
-parser.bind :port, :p, 'default - 8931'
+parser.comment :mac_port, 'default - 8931'
+parser.comment :android_port, 'default - 8930'
 parser.bind :help, :h, 'show help'
 @@first, @@params = parser.parse(ARGV)
 
 {
-  :port => 8931
+  :mac_port => 8931,
+  :android_port => 8930
 }.each{|k,v|
   @@params[k] = v unless @@params[k]
 }
 
 @@channel = EM::Channel.new
 
-class EchoServer < EM::Connection
+class MacServer < EM::Connection
   def post_init
     @sid = @@channel.subscribe{|mes|
       send_data mes
     }
-    puts "new client <#{@sid}>"
-    @@channel.push "new client <#{@sid}> connected\n"
+    puts "new mac <#{@sid}>"
+    @@channel.push "new mac <#{@sid}> connected\n"
   end
 
   def receive_data(data)
@@ -39,7 +42,28 @@ class EchoServer < EM::Connection
   end
 end
 
+class AndroidServer  < EventMachine::Connection
+  include EventMachine::HttpServer
+ 
+  def process_http_request
+    res = EventMachine::DelegatedHttpResponse.new(self)
+
+    puts Time.now
+    puts "request_method : #{@http_request_method}"
+    puts "path_info : #{@http_path_info}"
+    puts "query_str : #{@http_query_string}"
+    puts "post_content : #{@http_post_content}"
+
+    res.status = 200
+    res.content = "android server"
+    res.send_response
+  end
+end
+
 EM::run do
-  puts "GoldFish server start - port:#{@@params[:port].to_i}"
-  EM::start_server('0.0.0.0', @@params[:port].to_i, EchoServer)
+  puts "GoldFish server start"
+  puts "mac server - port:#{@@params[:mac_port].to_i} - HTTP"
+  EM::start_server('0.0.0.0', @@params[:mac_port].to_i, MacServer)
+  puts "android server - port:#{@@params[:android_port].to_i} - TCP Socket"
+  EM::start_server('0.0.0.0', @@params[:android_port].to_i, AndroidServer)
 end
