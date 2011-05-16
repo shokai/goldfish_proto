11c6fbbd112a06ad359ab942d4a3a65719d7c506
Sho Hashimoto <hashimoto@shokai.org>
2011-05-16 10:55:04 +0900

    send clipboard to all mac clients

--- server/goldfish_server.rb
+++ server/goldfish_server.rb
@@ -7,7 +7,6 @@ require 'uri'
 require 'json'
 
 parser = ArgsParser.parser
-
 parser.comment :mac_port, 'default - 8931'
 parser.comment :android_port, 'default - 8930'
 parser.bind :help, :h, 'show help'
@@ -22,6 +21,7 @@ parser.bind :help, :h, 'show help'
 
 @@channel = EM::Channel.new
 @@clips = Hash.new
+@@clipboard = ''
 
 class MacServer < EM::Connection
   def post_init
@@ -60,23 +60,38 @@ class AndroidServer  < EventMachine::Connection
     puts Time.now
     puts "request_method : #{@http_request_method}"
     puts "path_info : #{@http_path_info}"
+
     puts "query_str :"
-    p query = Hash[*(@http_query_string.to_s.split('&').map{|i|
-                            j = i.split('=')
-                            [j[0].to_sym, URI.decode(j[1])]
-                          }).flatten]
+    begin
+      p query = Hash[*(@http_query_string.to_s.split('&').map{|i|
+                         j = i.split('=')
+                         [j[0].to_sym, URI.decode(j[1])]
+                       }).flatten]
+    rescue => e
+      query = {}
+      STDERR.puts e
+      STDERR.puts 'http_query_string parse error'
+    end
 
     puts "post_content :"
+    begin
     p post_content = Hash[*(@http_post_content.to_s.split('&').map{|i|
                               j = i.split('=')
                               [j[0].to_sym, URI.decode(j[1])]
                             }).flatten]
-    
-    
+    rescue => e
+      post_content = {}
+      STDERR.puts e
+      STDERR.puts 'http_post_content parse error'
+    end
+
     res.status = 200
     res.content = ''
     if post_content[:action] == 'copy'
-      res.content = @@clips[post_content[:tag]]
+      @@clipboard = @@clips[post_content[:tag]]
+      res.content = @@clipboard
+    elsif post_content[:action] == 'paste'
+      @@channel.push({:clip => @@clipboard}.to_json)
     end
     res.send_response
   end
