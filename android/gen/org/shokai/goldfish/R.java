0ebeb7f8ef6a70e963064069fc78f8e977bb7979
Sho Hashimoto <hashimoto@shokai.org>
2011-05-16 17:54:54 +0900

    put top.png

--- android/gen/org/shokai/goldfish/R.java
+++ android/gen/org/shokai/goldfish/R.java
@@ -12,8 +12,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int top=0x7f020001;
     }
     public static final class id {
+        public static final int imageViewTop=0x7f050001;
         public static final int textViewTag=0x7f050000;
     }
     public static final class layout {
