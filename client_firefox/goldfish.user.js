813a855755aa129b4b70fa90eeaef31393c1fe27
Sho Hashimoto <hashimoto@shokai.org>
2011-05-17 14:04:56 +0900

    firefox greasemoneky client

---
+++ client_firefox/goldfish.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name           goldfish
+// @namespace      http://masui.sfc.keio.ac.jp/
+// @description    send URL
+// @include        http://*
+// @version        0.0.1
+// @author         Sho Hashimoto <hashimoto@shokai.org>
+// ==/UserScript==
+
+var goldfish = function(){
+    // var api_url = "http://localhost:8930";
+    var api_url = "http://dev.shokai.org:8930";
+
+    var nfc_tag = "011a0005150dc715"; // shokai
+    // var nfc_tag = "d36309c7";
+    // var nfc_tag = "234609c7";
+    // var nfc_tag = "535209c7";
+
+    GM_xmlhttpRequest({
+        method: "POST",
+        url: api_url+'/browser',
+        data: "url="+encodeURIComponent(location.href) +
+            "&title="+encodeURIComponent(document.title) +
+            "&tag="+encodeURIComponent(nfc_tag),
+        onload: function(response) {
+        }
+    });
+}();
